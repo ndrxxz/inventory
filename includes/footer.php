b64a21a774ac09f7727c249153e5a301a5e79695
Andrea <80144630+ndrxxz@users.noreply.github.com>
2024-08-24 13:57:48 +0800

    updated

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
-            <footer class="footer">
+            <footer class="footer fixed-bottom bottom-0 z-1">
                 <div class="container-fluid w-100">
                     <div class="row">
-                        <div class="col-12 text-end bg-secondary-subtle p-3">
+                        <div class="col-12 text-center bg-secondary-subtle p-2">
                             <h6>Copyright &copy; 2024 | Created with ❤️ by Andrea</h6>
                         </div>
                     </div>
@@ -11,5 +11,40 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="../src/js/script.js"></script>
+
+    <!--sweet alert-->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        $(document).ready(function(){
+            $('.del').on('click', function(e) {
+                e.preventDefault();
+                const href = $(this).attr('href')
+
+                Swal.fire({
+                    title : 'Are you sure?',
+                    text : 'This record will be deleted permanently!',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes'
+                }).then((result) => {
+                    if(result.value){
+                        document.location.href = href;
+                    }
+                })
+            });
+        });
+    </script>
+
+    <!--data tables-->
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+        $(document).ready( function () {
+            $('#myTable').DataTable();
+        });
+    </script>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../src/css/styles.css">
